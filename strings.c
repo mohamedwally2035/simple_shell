2a64f55e81821f8f333327713469857e02621201
mohamedwally2035 <mohamedwally87@gmail.com>
2024-02-28 18:45:17 -0800

    simple-shell

--- strings.c
+++ strings.c
@@ -26,7 +26,7 @@ int _strle(const char *s)
  *
  * Return: Negative if s1 < s2, positive if s1 > s2, zero if s1 == s2.
  */
-int _strcm(const char *s1, const char *s2)
+int _strcmp(const char *s1, const char *s2)
 {
     while (*s1 && *s2)
     {
@@ -66,7 +66,7 @@ char *starts_with(const char *haystack, const char *needle)
  *
  * Return: Pointer to the destination buffer.
  */
-char *_strca(char *dest, const char *src)
+char *_strcat(char *dest, const char *src)
 {
     char *ret = dest;
 
