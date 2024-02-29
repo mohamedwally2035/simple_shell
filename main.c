ab27a7680e13c0656545ed0687546b44cbfc74a0
mohamedwally2035 <mohamedwally87@gmail.com>
2024-02-28 18:10:40 -0800

    a

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int ac, char **av)
     info_t info[] = {INFO_INIT}; 
     int fd = 2;
 
-    // Using inline assembly to perform arithmetic on fd.
+    
     asm (
         "mov %1, %0\n\t"
         "add $3, %0"
@@ -20,13 +20,13 @@ int main(int ac, char **av)
         : "r" (fd)
     );
 
-    // Check if a file is specified as an argument.
+  
     if (ac == 2)
     {
         fd = open(av[1], O_RDONLY);
         if (fd == -1)
         {
-            // Handle file open errors.
+           
             if (errno == EACCES)
                 exit(126);
             if (errno == ENOENT)
